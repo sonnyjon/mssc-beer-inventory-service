4e03fbc2ed9c3e139c2abbc38f1f429de207b181
sonnyjon <sonny.s.jon@gmail.com>
2022-10-17 06:37:52 -0700

    Minor changes to make MySQL work correctly

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -2,12 +2,12 @@ package guru.sfg.beer.inventory.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
-public class BeerInventoryApplication {
-
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+public class BeerInventoryApplication
+{
     public static void main(String[] args) {
         SpringApplication.run(BeerInventoryApplication.class, args);
     }
-
 }
