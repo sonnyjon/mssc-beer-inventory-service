934cdbe96088ab4f7cde64608117f7ffc047b834
sonnyjon <sonny.s.jon@gmail.com>
2022-10-23 08:55:22 -0700

    JMS configuration for microservice to send and receive messages.

---
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -0,0 +1,30 @@
+package guru.sfg.beer.inventory.service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+
+/**
+ * Created by Sonny on 10/23/2022.
+ */
+@Configuration
+public class JmsConfig
+{
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
+    public static final String DEALLOCATE_ORDER_QUEUE = "deallocate-order";
+
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper)
+    {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType( MessageType.TEXT );
+        converter.setTypeIdPropertyName( "_type" );
+        converter.setObjectMapper( objectMapper );
+        return converter;
+    }
+}
